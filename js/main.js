ba99b8743eba2b20a2b8ee907d0c944a98e00319
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-09-11 15:35:04 -0300

    js switch theme

--- js/main.js
+++ js/main.js
@@ -1,54 +1,76 @@
 var teclado = document.querySelector('#teclado');
 var visor = document.querySelector('#visor');
 var valores = [];
-teclado.addEventListener('click' , e =>{
+teclado.addEventListener('click', e => {
 
-   let dataAtribute = e.target.dataset.tipo;
-   let valueAlvo = e.target.value;
-    if(dataAtribute === 'numerico'){
+    let dataAtribute = e.target.dataset.tipo;
+    let valueAlvo = e.target.value;
+    if (dataAtribute === 'numerico') {
         visor.textContent += valueAlvo;
-    }else if(dataAtribute === 'funcional'){
+    } else if (dataAtribute === 'funcional') {
         verificaPonto = new RegExp(/\./)
-        if(valueAlvo === '.' && !verificaPonto.test(visor.textContent)){
+        if (valueAlvo === '.' && !verificaPonto.test(visor.textContent)) {
             visor.textContent += valueAlvo;
-        }else if(visor.textContent != '' && visor.textContent != '-' && (valueAlvo === '+' || valueAlvo === '/' || valueAlvo === 'x' || valueAlvo === '-' ) ){
-            if(valueAlvo === 'x'){
+        } else if (visor.textContent != '' && visor.textContent != '-' && (valueAlvo === '+' || valueAlvo === '/' || valueAlvo === 'x' || valueAlvo === '-')) {
+            if (valueAlvo === 'x') {
                 valueAlvo = '*';
             }
-            valores.push(visor.textContent,valueAlvo);
+            valores.push(visor.textContent, valueAlvo);
             limpaVisor();
-      
-        }else if(valueAlvo === '-' && visor.textContent === ''){
+
+        } else if (valueAlvo === '-' && visor.textContent === '') {
             visor.textContent += valueAlvo;
-        }else if (valueAlvo === '='){
-            if(visor.textContent!= ''){
+        } else if (valueAlvo === '=') {
+            if (visor.textContent != '') {
                 valores.push(visor.textContent);
             }
             resultadoTexto = resultado(valores);
             visor.textContent = resultadoTexto;
             reset();
-        }else if(valueAlvo === 'RESET'){
+        } else if (valueAlvo === 'RESET') {
             reset();
             limpaVisor();
-        }else if(valueAlvo === 'DEL' && valueAlvo != ''){
-         
+        } else if (valueAlvo === 'DEL' && valueAlvo != '') {
+
             let valor = visor.textContent;
-            valor = valor.substring(0,valor.length - 1)
+            valor = valor.substring(0, valor.length - 1)
             visor.textContent = valor;
         }
-   
+
 
     }
 })
 
-function limpaVisor(){
+function limpaVisor() {
     visor.textContent = '';
 }
-function reset(){
+function reset() {
     valores = [];
 }
-function resultado(numeroDigitados){
-  
+function resultado(numeroDigitados) {
+
     return eval(numeroDigitados.join(''))
- 
-} 
+
+}
+
+temas = document.querySelector('#input-radio');
+
+temas.addEventListener('input', e => {
+    let html = document.querySelector('html')
+    
+    if (e.target.value === '1') {
+        html.classList.remove('themeDois')
+        html.classList.remove('themeTres')
+        html.classList.add('themeUm')
+    } else if (e.target.value === '2') {
+        html.classList.add('themeDois')
+        html.classList.remove('themeTres')
+        html.classList.remove('themeUm')
+    } else if (e.target.value === '3') {
+        html.classList.remove('themeDois')
+        html.classList.add('themeTres')
+        html.classList.remove('themeUm')
+    }
+
+
+})
